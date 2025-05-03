f73ba882555319c63d2ce0427028ff05cea8cbb7
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-05-02 20:55:28 -0400

    feat: firestore test connection

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,29 @@
-import { LayoutStyles } from '@/components/LayoutStyle';
-import {  StyleSheet, Platform, View } from 'react-native';
-import { Text } from 'react-native';
+import { LayoutStyles } from "@/components/LayoutStyle";
+import { StyleSheet, View } from "react-native";
+import { Text } from "react-native";
+import {
+  collection,
+  query,
+  getDocs,
+  getFirestore,
+} from "@react-native-firebase/firestore";
+import { useEffect } from "react";
 
 export default function HomeScreen() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const db = getFirestore();
+      const q = query(collection(db, "test"));
+      const querySnapshot = await getDocs(q);
+
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+      });
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View style={LayoutStyles.container}>
       <Text style={LayoutStyles.Title__text}>Lecciones</Text>
@@ -10,12 +31,12 @@ export default function HomeScreen() {
   );
 }
 
-const styles=  StyleSheet.create({
-  HomeScreen__Container:{
+const styles = StyleSheet.create({
+  HomeScreen__Container: {
     flex: 1,
-    backgroundColor: '#3d3d3d',
-    width: '100%',
-    height: '100%',
+    backgroundColor: "#3d3d3d",
+    width: "100%",
+    height: "100%",
     padding: 20,
-  }
-})
+  },
+});
