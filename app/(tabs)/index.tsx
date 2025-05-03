56866196a80bffee6108dc95bd28793677a5623e
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-05-03 12:42:09 -0400

    feat: get resource from firebase storage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,22 @@
 import { LayoutStyles } from "@/components/LayoutStyle";
 import { StyleSheet, View } from "react-native";
 import { Text } from "react-native";
+import { Image } from "expo-image";
 import {
   collection,
   query,
   getDocs,
   getFirestore,
 } from "@react-native-firebase/firestore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getStorage } from "@react-native-firebase/storage";
 
 export default function HomeScreen() {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
   useEffect(() => {
     const fetchData = async () => {
+      // Firestore get document query
       const db = getFirestore();
       const q = query(collection(db, "test"));
       const querySnapshot = await getDocs(q);
@@ -19,14 +24,27 @@ export default function HomeScreen() {
       querySnapshot.forEach((doc) => {
         console.log(doc.id, " => ", doc.data());
       });
-    };
 
+      // Storage get resource query
+      const storage = getStorage();
+      const ref = storage.ref("/libro-minedu/portada-libro-minedu.png");
+      const url = await ref.getDownloadURL();
+      console.log(url);
+      setImageUrl(url);
+    };
     fetchData();
   }, []);
 
   return (
     <View style={LayoutStyles.container}>
       <Text style={LayoutStyles.Title__text}>Lecciones</Text>
+      {imageUrl && (
+        <Image
+          source={{ uri: imageUrl }}
+          contentFit="cover"
+          style={{ width: 200, height: 200 }}
+        />
+      )}
     </View>
   );
 }
