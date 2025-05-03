8f4e05b747f0994586ff50f4bc98baece6aa47b2
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-05-02 20:53:26 -0400

    feat: add react native firebase & firebase/firestore libraries

---
+++ app.config.ts
@@ -0,0 +1,47 @@
+import { ExpoConfig, ConfigContext } from "@expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  name: config.name ?? "ensenas",
+  slug: config.slug ?? "ensenas",
+  version: config.version ?? "1.0.0",
+  orientation: config.orientation ?? "portrait",
+  icon: config.icon ?? "./assets/images/icon.png",
+  scheme: config.scheme ?? "myapp",
+  userInterfaceStyle: config.userInterfaceStyle ?? "automatic",
+  newArchEnabled: config.newArchEnabled ?? true,
+  ios: config.ios ?? { supportsTablet: true },
+  android: {
+    ...config.android,
+    googleServicesFile:
+      process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
+  },
+  web: config.web ?? {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: config.plugins ?? [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+    ],
+  ],
+  experiments: config.experiments ?? {
+    typedRoutes: true,
+  },
+  extra: config.extra ?? {
+    router: {
+      origin: false,
+    },
+    eas: {
+      projectId: "62cfe78c-67f1-4397-af0b-a2bea4a7c425",
+    },
+  },
+  owner: config.owner ?? "polvosmagicos",
+});
