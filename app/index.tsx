70cefd20047b0d3d1407138108ce0def508e5c36
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-06-08 21:49:28 -0400

    feat: add auth state

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,45 @@
 import { useEffect, useState } from "react";
-import { View, Text, ActivityIndicator, StatusBar } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import { useRouter } from "expo-router";
-import "../i18n/i18n"
+import "../i18n/i18n";
+import {
+  FirebaseAuthTypes,
+  getAuth,
+  onAuthStateChanged,
+} from "@react-native-firebase/auth";
 
 export default function Index() {
   const router = useRouter();
-  const [user, setUser] = useState<null | object | undefined>(undefined); // undefined means loading
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  // Handle user state changes
+  function handleAuthStateChanged(user: FirebaseAuthTypes.User | null) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
 
   useEffect(() => {
-    const checkAuth = async () => {
-      // Simulate async auth logic
-      const fetchedUser = null;
-      setUser(fetchedUser);
-    };
-
-    checkAuth();
+    const subscriber = onAuthStateChanged(getAuth(), handleAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
   }, []);
 
   useEffect(() => {
-    if (user === undefined) return;
+    if (initializing) return;
 
     if (user) {
       router.replace("/(tabs)");
     } else {
       router.replace("/auth");
     }
-  }, [user]);
+  }, [user, initializing, router]);
 
   return (
     <>
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <ActivityIndicator size="large" />
-      <Text>Checking authentication...</Text>
-    </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+        <Text>Checking authentication...</Text>
+      </View>
     </>
   );
 }
