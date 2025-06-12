c338976f87d9f331656f295527a4843a5a7072db
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-06-12 16:48:10 -0400

    feat: badges tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,34 @@
 import { Tabs } from "expo-router";
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import Entypo from '@expo/vector-icons/Entypo';
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Entypo from "@expo/vector-icons/Entypo";
 import { useColorScheme } from "nativewind";
 
-
 export default function TabLayout() {
-  const {colorScheme} = useColorScheme()
+  const { colorScheme } = useColorScheme();
   return (
     <Tabs
       screenOptions={{
         /* tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint, */
         headerShown: false,
-        tabBarStyle: { 
+        tabBarStyle: {
           marginTop: 0,
           padding: 0,
-          backgroundColor: colorScheme==='dark'? '#24262f' : 'white',
-          borderColor: 'rgba(137, 137, 137, 0.2)',
-         },
+          backgroundColor: colorScheme === "dark" ? "#24262f" : "white",
+          borderColor: "rgba(137, 137, 137, 0.2)",
+        },
       }}
     >
       <Tabs.Screen
         name="home"
         options={{
-          title: '',
+          title: "",
           tabBarIcon: ({ color }) => (
-            <Entypo name="home" size={24} color={colorScheme === 'dark' ? 'white': 'black'} />
+            <Entypo
+              name="home"
+              size={24}
+              color={colorScheme === "dark" ? "white" : "black"}
+            />
           ),
           /* tabBarIcon: ({ color }) => <IconSymbol size={28} name="book.fill" color={color} />, */
         }}
@@ -33,9 +36,13 @@ export default function TabLayout() {
       <Tabs.Screen
         name="dictionary"
         options={{
-          title: '',
+          title: "",
           tabBarIcon: ({ color }) => (
-            <FontAwesome name="book" size={24} color={colorScheme === 'dark' ? 'white': 'black'} />
+            <FontAwesome
+              name="book"
+              size={24}
+              color={colorScheme === "dark" ? "white" : "black"}
+            />
           ),
           /* tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />, */
         }}
@@ -43,20 +50,43 @@ export default function TabLayout() {
       <Tabs.Screen
         name="translator"
         options={{
-          title: '',
+          title: "",
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="translate" size={24} color={colorScheme === 'dark' ? 'white': 'black'} />
+            <MaterialIcons
+              name="translate"
+              size={24}
+              color={colorScheme === "dark" ? "white" : "black"}
+            />
           ),
 
           /* tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />, */
         }}
       />
       <Tabs.Screen
+        name="badges"
+        options={{
+          title: "",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons
+              name="military-tech"
+              size={24}
+              color={colorScheme === "dark" ? "white" : "black"}
+            />
+          ),
+          /* tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />, */
+        }}
+      />
+
+      <Tabs.Screen
         name="settings"
         options={{
-          title: '',
+          title: "",
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="account-circle" size={24} color={colorScheme === 'dark' ? 'white': 'black'} />
+            <MaterialIcons
+              name="account-circle"
+              size={24}
+              color={colorScheme === "dark" ? "white" : "black"}
+            />
           ),
           /* tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />, */
         }}
@@ -64,4 +94,3 @@ export default function TabLayout() {
     </Tabs>
   );
 }
-
