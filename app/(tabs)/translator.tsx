47b66a9cf35e0850387c11f4b7ca0fbfa406e88e
CuetoSDev <cuetosalazarsebas@gmail.com>
2025-07-02 18:47:04 -0500

    tts

---
+++ app/(tabs)/translator.tsx
@@ -0,0 +1,29 @@
+/* import { View, StyleSheet, Button } from 'react-native';
+import * as Speech from 'expo-speech';
+
+tallback desactivado hasta sgte build
+
+
+
+export default function App() {
+  const speak = () => {
+    const thingToSay = 'new type of hero';
+    Speech.speak(thingToSay);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Press to hear some words" onPress={speak} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+});
+ */
